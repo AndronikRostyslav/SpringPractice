42ad0381b8ebb5ff0395c443530390e282eced5d
Anastasiia Samchuk <samchuk.anastasiia1@gmail.com>
2024-05-20 19:46:41 +0300

    update models (context, movie_genre, schedule)

--- practice/Models/Movie_Genre.cs
+++ practice/Models/Movie_Genre.cs
@@ -11,4 +11,20 @@
             this.genre_id = genre_id;
         }
     }
+
+    public class Movie_Genre_Info
+    {
+        public int movie_id { get; set;}
+        public string title { get; set;}
+        public string genre_name { get; set;}
+
+        public Movie_Genre_Info (int movie_id, string title, string genre_name)
+        {
+            this.movie_id = movie_id;
+            this.title = title;
+            this.genre_name = genre_name;
+        }
+    }
 }
+
+
